f88ec193e110db597b09ffd1b0c6338729b3b0b9
Manishraii-web <raimanish489@gmail.com>
2026-05-23 16:38:47 +0545

    more python prac

---
+++ Classroom_Learning/Loops/atmpinCHecker.py
@@ -0,0 +1,14 @@
+PIN = 1234
+attempts = 0
+success = False
+
+while attempts < 3:
+    guess = int(input(f"Attempt{attempts+1}: "))
+    attempts+=1
+    if guess == PIN:
+        success = True
+        break
+if success:
+    print("Access granted")
+else:
+    print("Account locked")
