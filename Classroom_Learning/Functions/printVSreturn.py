85c5fd62480e16300fe6ec2ac36a06bca11f27d5
Manishraii-web <raimanish489@gmail.com>
2026-05-26 09:34:13 +0545

    retun vs print()

---
+++ Classroom_Learning/Functions/printVSreturn.py
@@ -0,0 +1,16 @@
+# Using print() - Broken
+def add(a,b):
+    print(a + b) 
+
+result = add(3,4)
+print(result) 
+# final = result + 10  # Crash Out
+# print(final)
+
+# Using Return = Correct'
+def Add(x,y):
+    return x + y
+res = add (3,5)
+print(res)
+# final = res + 10  # error to tackle
+# print(final)
