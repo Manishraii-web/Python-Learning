5a24f4b6e260a54e5b54f51e19de45dc6f89488b
Manishraii-web <raimanish489@gmail.com>
2026-06-02 09:28:03 +0545

    msplit and whitespace

---
+++ Classroom_Learning/StringPYTHON/upperCase.py
@@ -0,0 +1,14 @@
+name = "villain Sama"
+name_upper = name.upper()
+print(name_upper)
+
+#removing whitespace
+namee = "    Manish Sama    "
+name1 = namee.strip()
+print(name1)
+
+#split
+nums  =["a,b,c"]
+newnums = nums[0].split(",")
+print(newnums)
+
