3cc904afae23fd5e4fbce85dd131d7ad30f25ddc
Manishraii-web <raimanish489@gmail.com>
2026-06-02 09:19:27 +0545

    mindexing file

--- Classroom_Learning/StringPYTHON/indexing.py
+++ Classroom_Learning/StringPYTHON/indexing.py
@@ -1,6 +1,9 @@
 text = "Hello Hunny Bunny"
-print(text[0:6])  # H
-print(text[4:])
-print(text[:5])
-print(text[::2])
-print(text[::-4])
+
+
+print(text[0:6])   #0-5
+print(text[4:]) #after 4
+print(text[:5]) #frist 5
+print(text[::2])  #skip 2
+print(text[::-4]) # reverse
+
