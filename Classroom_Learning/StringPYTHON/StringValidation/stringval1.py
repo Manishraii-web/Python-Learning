a76eb23bf69681c66306999050ac9e89775c244a
Manishraii-web <raimanish489@gmail.com>
2026-06-04 07:46:32 +0545

    mnew files

---
+++ Classroom_Learning/StringPYTHON/StringValidation/stringval1.py
@@ -0,0 +1,5 @@
+text = 'Hello World'
+ print(text.isalpha())  # False, because of space
+ print(text.isdigit())  # False, because it contains letters and space
+ print(text.isalnum())  # False, because of space
+ print(text.islower())  # False, because of uppercase letters
